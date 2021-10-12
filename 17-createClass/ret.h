39436d2eee1a6ecdecd3b93cffb2d035e6da2f1e
Inna Larina <cool.inna-24@yandex.ru>
2021-10-12 11:35:46 +0300

    add contractility and zero-denumerator check

--- 17-createClass/ret.h
+++ 17-createClass/ret.h
@@ -3,14 +3,34 @@ using namespace std;
 
 class CRational {
     public:
-        float fract;
-        float set_fract(float num, float denum) {
+        int num;
+        int denum;
+
+        float set_fract(int num, int denum) { // ф-ция инициализации дроби числителем и знаменателем
+            if (denum == 0) {
+                cout << "Знаменатель = 0. Введите другое число" << endl;
+                return 0;
+            }
+            float fract;
             fract = num/denum;
             return fract;
-         }
+        }
 
-        void print(int num, int denum) {
+        void check (int &num, int &denum) { // процедура сокращения дроби
+            if (denum == 0) return;
+            int edge = denum;
+            if (num > denum) edge = num; 
+            for (int i = edge/2; i >= 2; i--) {
+                if ((num - i * (num / i) == 0) && (denum - i * (denum / i) == 0)) {
+                    num = num / i;
+                    denum = denum / i;
+                }
+            }
+        }
+
+        void print(int num, int denum) { // процедура вывода дроби (смешанной, правильной, целого числа)
             int x, d;
+            if (denum == 0) return; 
             if (num - denum * (num/denum) == 0){
                 cout << num/denum << endl;
             }
@@ -24,11 +44,20 @@ class CRational {
             }
         }
 
-        float sumFract(float a, float b) {
+        void sumFract(float a, float b) { // процедура сложения/умножения двух дробей
             float sum, mult;
             sum = a + b;
             mult = a * b;
+
             cout << sum << " " << mult << endl;
-            return sum;
         }   
 };
+
+/*
+class Complex{
+    public:
+        float re, im;
+        Complex(float _re, float _im): re(-re), im(-im) {} // {} - body is not needed. Exception - ;
+}
+(.cpp) Complex c(1,5)
+*/
